3474ff1d3639941b22a99a3808d3b03410c342a6
Sergej Shafarenka <sergej.shafarenka@halfbit.de>
2024-07-24 18:39:21 +0200

    Turn LogTag into an interface

--- logger/src/commonMain/kotlin/de/halfbit/logger/LogTag.kt
+++ logger/src/commonMain/kotlin/de/halfbit/logger/LogTag.kt
@@ -4,19 +4,26 @@ package de.halfbit.logger
 import de.halfbit.logger.LoggableLevel.Everything
 import kotlin.reflect.KClass
 
-public abstract class LogTag {
-    public abstract val name: String
-    public abstract val loggableLevel: LoggableLevel
+// public
+
+public sealed interface LogTag {
+    public val name: String
+    public val loggableLevel: LoggableLevel
 }
 
 public fun namedTag(name: String, loggableLevel: LoggableLevel = Everything): LogTag =
-    object : LogTag() {
-        override val name: String get() = name
-        override val loggableLevel: LoggableLevel get() = loggableLevel
-    }
+    DefaultLogTag(name, loggableLevel)
 
 public fun classTag(clazz: KClass<*>, loggableLevel: LoggableLevel = Everything): LogTag =
     namedTag(name = clazz.simpleName ?: "", loggableLevel = loggableLevel)
 
 public inline fun <reified T : Any> classTag(loggableLevel: LoggableLevel = Everything): LogTag =
     classTag(clazz = T::class, loggableLevel = loggableLevel)
+
+// private
+
+private class DefaultLogTag(
+    override val name: String,
+    override val loggableLevel: LoggableLevel
+) : LogTag
+
